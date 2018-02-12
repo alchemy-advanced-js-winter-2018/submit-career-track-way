6682746d379e100ed5d57f9bc4d4989bc5f6853c
CharlyWelch <blwbiology@gmail.com>
2018-02-12 10:50:43 -0800

    add subtract function and associated test

--- test/math.test.js
+++ test/math.test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const add = require('../lib/add');
+const subtract = require('../lib/subtract');
 
 describe('math functions', () => {
     it('add', () => {
         assert.equal(add(1, 2), 3);
+    });
+
+    it('subtract', () => {
+        assert.equal(subtract(5, 1), 4);
     });
 
 });
