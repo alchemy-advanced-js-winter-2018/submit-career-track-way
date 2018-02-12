37c8c0e5989809e2ef72c5b9d37ff7a5bd5d2f77
martypdx <marty@martypdx.com>
2018-02-12 10:46:23 -0800

    working add function

---
+++ test/math.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const add = require('../lib/add');
+
+describe('math functions', () => {
+
+    it('add', () => {
+        assert.equal(add(1, 2), 3);
+    });
+});
+
