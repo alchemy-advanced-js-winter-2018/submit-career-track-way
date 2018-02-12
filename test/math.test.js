8615db76d11c8f2785eecd8963082e9e9c8a899a
CharlyWelch <blwbiology@gmail.com>
2018-02-12 10:57:25 -0800

    add divide funtion and test

--- test/math.test.js
+++ test/math.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const add = require('../lib/add');
 const subtract = require('../lib/subtract');
 const multiply = require('../lib/multiply');
+const divide = require('../lib/divide');
 
 describe('math functions', () => {
     it('add', () => {
@@ -16,6 +17,12 @@ describe('math functions', () => {
         assert.equal(multiply(2, 4), 8);
     });
 
-    
+    it('divide', () => {
+        assert.equal(divide(4, 2), 2);
+    });
+
+    it('throws an error if dividing by zero', () => {
+
+    });
 
 });
