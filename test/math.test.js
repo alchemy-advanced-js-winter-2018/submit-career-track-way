1248fe0b8c33a68732d5004bf1f4c12dbb23879e
martypdx <marty@martypdx.com>
2018-02-12 10:52:31 -0800

    basic operators

--- test/math.test.js
+++ test/math.test.js
@@ -1,10 +1,29 @@
 const assert = require('assert');
 const add = require('../lib/add');
+const subtract = require('../lib/subtract');
+const multiply = require('../lib/multiply');
+const divide = require('../lib/divide');
 
 describe('math functions', () => {
 
     it('add', () => {
         assert.equal(add(1, 2), 3);
+    });
+
+    it('subtract', () => {
+        assert.equal(subtract(5, 2), 3);
+    });
+
+    it('multiply', () => {
+        assert.equal(multiply(2, 3), 6);
+    });
+
+    it('divide', () => {
+        assert.equal(divide(12, 3), 4);
+    });
+
+    it('throws an error dividing by zero', () => {
+
     });
 });
 
